b0e7b563d22bdc74f4e2ccdb26a02302bc01d45a
Vasenina Yuliya <mamicheva@gmail.com>
2022-01-29 19:19:27 +0100

    add file

--- client/src/member-page/addMember.js
+++ client/src/member-page/addMember.js
@@ -8,29 +8,75 @@ import InputField from "../ui/inputField";
 export default function AddMember({}) {
     const history = useHistory();
     const [values, handleChange] = useTextInput();
+    const [file, setFile] = useState();
 
     useEffect(() => {
         console.log("add-member component mounted");
     }, []);
 
+    const getPhoto = (e) => {
+        //console.log("user changed a photo");
+
+        console.log("fileselecthandler", e.target.files[0]);
+        setFile(e.target.files[0]);
+    };
+    const addingUserToRedux = (member) => {
+        //
+        console.log("server sent me this member: ", member);
+
+        location.assign(`/member/${member.id}`);
+    };
+
     const addMemberClick = (e) => {
         e.preventDefault();
         console.log("add member", values);
 
-        fetch("/api/add-member", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(values),
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log("AddMember:data from server", data);
+        if (file) {
+            const fd = new FormData();
+            fd.append("file", file);
+            if (values.first) {
+                fd.append("first", values.first);
+            }
+            if (values.last) {
+                fd.append("last", values.last);
+            }
+
+            fetch("/api/add-member-and-photo", {
+                method: "POST",
+                body: fd,
             })
-            .catch((err) => {
-                console.log("error addMember", err);
-            });
+                .then((res) => {
+                    res.json();
+                })
+                .then((data) => {
+                    if (data.success) {
+                        addingUserToRedux(data.newMember);
+
+                        //add a user to redux
+                    }
+                    console.log(data);
+                })
+                .catch((err) => {
+                    console.log("error", err);
+                });
+        } else {
+            fetch("/api/add-member", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(values),
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log("AddMember:data from server", data);
+                    addingUserToRedux(data.newMember);
+                    //add a user to redux
+                })
+                .catch((err) => {
+                    console.log("error addMember", err);
+                });
+        }
     };
 
     return (
@@ -38,6 +84,12 @@ export default function AddMember({}) {
             Add Member
             <form>
                 <MemberPic size="big" />
+                <input
+                    className="input-file"
+                    type="file"
+                    accept="image/*"
+                    onChange={getPhoto}
+                />
                 <InputField
                     label="last"
                     name="last"
--- server/routers/member.js
+++ server/routers/member.js
@@ -3,6 +3,30 @@ const member = express.Router();
 
 const db = require("../db/db");
 
+const s3 = require("../s3");
+
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, path.join(__dirname, "uploads"));
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
+
 member.post("/api/add-member", async (req, res) => {
     console.log(req.body);
     let newMember = req.body;
@@ -18,5 +42,33 @@ member.post("/api/add-member", async (req, res) => {
         return;
     }
 });
+
+member.post(
+    "/api/add-member-and-photo",
+    uploader.single("file"),
+    s3.upload,
+    async (req, res) => {
+        console.log("user wants to add member with photo");
+        if (!req.file) {
+            res.json({ success: false });
+            return;
+        } else {
+            const url = s3.getLink(req.file.filename);
+            console.log(req.body);
+            let newMember = { ...req.body, image_url: url };
+            console.log("new member with photo", newMember);
+            try {
+                const member_id = await db.addMember(newMember);
+                newMember.id = member_id;
+                res.json({ success: true, newMember });
+                return;
+            } catch (err) {
+                console.log("error in adding member with photo", err);
+                res.status(500).json({ success: false });
+                return;
+            }
+        }
+    }
+);
 
 module.exports.member = member;
